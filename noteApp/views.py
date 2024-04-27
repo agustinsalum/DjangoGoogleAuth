591ac41704053a9f0ea0e52d985331d6d625215f
Agustin Salum <agustinsalum92@hotmail.com>
2024-04-27 18:46:03 -0300

    Realizado el listado de usuarios y la paginacion

--- noteApp/views.py
+++ noteApp/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic import TemplateView, ListView, View
-
-from django.shortcuts import redirect
 
 from .forms import LoginForm, RegistrationForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-
 from django.contrib import messages
-
 
 from django.utils.decorators import method_decorator
 
+from .models import UserProfile
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -53,8 +52,25 @@ class RegisterView(View):
 
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = UserProfile
     template_name = 'home.html'
+    context_object_name = 'user_profiles'
+    paginate_by = 2
+
+    # Added the get method to handle out-of-range numbers or other issues
+    def get(self, request, *args, **kwargs):
+        user_profiles = self.get_queryset()
+        paginator = Paginator(user_profiles, self.paginate_by)
+        page_number = request.GET.get('page', 1)
+
+        try:
+            page_obj = paginator.get_page(page_number)
+        except PageNotAnInteger:
+            page_obj = paginator.page(1)
+        except EmptyPage:
+            page_obj = paginator.page(paginator.num_pages)
+        return render(request, self.template_name, {'page_obj': page_obj})
 
 class CustomLogoutView(View):
     def post(self, request):
